eae32df554249deecf87966c734f64dc06c5842b
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-12-06 21:31:51 +0200

    Solution to coding challeng #1

---
+++ script.js
@@ -0,0 +1,27 @@
+let markMass = 78;
+let markHeight = 1.69;
+
+let johnMass = 92;
+let johnHeight = 1.95;
+
+let markBMI = markMass / (markHeight ** 2);
+
+console.log("Mark BMI = ", markBMI);
+
+let johnBMI = johnMass / (johnHeight * johnHeight);
+
+console.log("John BMI = ", markBMI);
+
+markMass = 95;
+markHeight = 1.88;
+
+johnMass = 85;
+markHeight = 1.76;
+
+markBMI = markMass / (markHeight ** 2);
+
+console.log("Mark BMI = ", markBMI);
+
+johnBMI = johnMass / (johnHeight * johnHeight);
+
+console.log("John BMI = ", markBMI);
